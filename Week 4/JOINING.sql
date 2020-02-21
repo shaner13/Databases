3a8156cdfd6e27765fdc62b92816bd5de6b13e85
shaner13 <43356131+shaner13@users.noreply.github.com>
2020-02-21 12:19:58 +0000

    Update JOINING.sql

--- Week 4/JOINING.sql
+++ Week 4/JOINING.sql
@@ -1,11 +1,11 @@
 alter session  set current_schema = builder2;
 
-SELECT A, B FROM A JOIN B USING (COMMON ELEMENT);
+SELECT A, B FROM A JOIN B USING (COMMON ATTRIBUTE);
 
 --1
-SELECT Coderno, Order_Date, Customer_Name
+SELECT Corderno, Order_Date, Customer_Name
 FROM Corder 
-JOIN Customer USING (Customer_ID);
+JOIN Customer USING (Customer_Id);
 
 --2 
 SELECT Supplier_Name, Stock_Description 
@@ -13,10 +13,15 @@ FROM Supplier
 JOIN Stock USING (Supplier_ID);
 
 --3 ??
-SELECT Customer_Name, Stock_Description FROM Supplier JOIN Stock USING ()
-DISTINCT ORDER BY CUSTOMER_NAME
+SELECT DISTINCT Customer_Name, Stock_Description 
+FROM Customer 
+JOIN Corder USING (Customer_ID)
+JOIN Corderline USING (CORDERNO)
+JOIN Stock USING (Stock_Code)
+ORDER BY Customer_Name;
 
 --4
-SELECT Customer_Name, Corder 
+SELECT Customer_Name, COUNT(CORDERNO)
 FROM Customer 
-JOIN Corder USING (Customer_ID);
+JOIN Corder USING (Customer_id)
+GROUP BY Customer_Name;
